ed86304e6915c7f1abdde1c217077c56eb1b4c8b
Ford Combs <fcombs90@gmail.com>
2025-01-03 16:41:37 -0500

    bug fixes

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,6 +1,6 @@
 import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
-import { stripe } from '@/config/stripe';
+import { getStripe } from '@/config/stripe';
 import { clerkClient } from '@clerk/nextjs/server';
 
 export async function POST(req: Request) {
@@ -10,6 +10,7 @@ export async function POST(req: Request) {
   let event;
 
   try {
+    const stripe = getStripe();
     event = stripe.webhooks.constructEvent(
       body,
       signature!,
